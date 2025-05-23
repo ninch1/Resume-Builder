eb42273bad4affe03071948b970f86385afed171
Elguja Modebadze <gujiko.modebadze@gmail.com>
2025-05-23 13:12:00 +0400

    initial commit: setup vite, tailwind and react router.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center text-5xl">
+      <h1>Home</h1>
+      <p>Welcome to the home page!</p>
+    </div>
+  );
+}
